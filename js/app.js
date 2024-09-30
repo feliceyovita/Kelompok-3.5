5f3f1eb3583eb3ea1de86728d41793660474f5b4
chyntiaclaudiaa <chyntiaclaudia1809@gmail.com>
2024-09-30 14:13:51 +0700

    menyelesaikan halaman login/register

--- js/app.js
+++ js/app.js
@@ -60,3 +60,13 @@ thumbnails.forEach((thumbnail, index) => {
     })
 })
 
+// navbar scrolled
+const navE1 = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 56) {
+        navE1.classList.add('navbar-scrolled');
+    } else {
+        navE1.classList.remove('navbar-scrolled');
+    }
+});
---
+++ js/login.js
@@ -0,0 +1,26 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".auth-page-container"); 
+
+sign_up_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode");
+});
+
+function togglePassword(passwordId, iconId) {
+    const passwordInput = document.getElementById(passwordId);
+    const toggleIcon = document.getElementById(iconId);
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text"; 
+        toggleIcon.classList.remove("fa-eye"); 
+        toggleIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordInput.type = "password"; 
+        toggleIcon.classList.remove("fa-eye-slash"); 
+        toggleIcon.classList.add("fa-eye"); 
+    }
+}
