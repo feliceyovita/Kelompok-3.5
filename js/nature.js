f144c8764af464605d275ca546469434d02cf5b6
chyntiaclaudiaa <chyntiaclaudia1809@gmail.com>
2024-10-07 13:59:38 +0700

    menyelesaikan nature destination

---
+++ js/nature.js
@@ -0,0 +1,39 @@
+// Load More functionality
+let loadMoreBtn = document.getElementById('loadMoreBtn');
+let discoverCards = document.querySelectorAll('.discover__card');
+let visibleCards = 8; 
+
+// Function to show discover cards
+function showDiscoverCards() {
+    discoverCards.forEach((card, index) => {
+        card.style.display = 'none';
+    });
+
+    for (let i = 0; i < visibleCards; i++) {
+        if (discoverCards[i]) {
+            discoverCards[i].style.display = 'block';
+        }
+    }
+
+    if (visibleCards >= discoverCards.length) {
+        loadMoreBtn.style.display = 'none';
+    }
+}
+
+showDiscoverCards();
+
+loadMoreBtn.addEventListener('click', () => {
+    visibleCards += 8; 
+    showDiscoverCards(); 
+});
+
+// navbar scrolled
+const navE1 = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 56) {
+        navE1.classList.add('navbar-scrolled');
+    } else {
+        navE1.classList.remove('navbar-scrolled');
+    }
+});
