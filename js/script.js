379ce654ed98806b57e9cb92bd764006a184814a
feliceyovita <feliceyovitaaa@gmail.com>
2024-10-13 17:58:28 +0700

    menyelesaikan sliding card di index

---
+++ js/script.js
@@ -0,0 +1,69 @@
+const carousels = document.querySelectorAll(".wikit-carousel .wikit-carousel__carousel");
+const arrowIcons = document.querySelectorAll(".wikit-carousel__wrapper i");
+
+carousels.forEach((carousel, index) => {
+    let firstImg = carousel.querySelectorAll("img")[0];
+    let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+
+    const showHideIcons = () => {
+        // Show/hide prev/next icon according to carousel scroll left value
+        let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // Getting max scrollable width
+        arrowIcons[index * 2].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+        arrowIcons[index * 2 + 1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+    }
+
+    arrowIcons[index * 2].addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14; // Getting first img width & adding 14 margin value
+        carousel.scrollLeft -= firstImgWidth; // Scroll to the left
+        setTimeout(() => showHideIcons(), 60);
+    });
+
+    arrowIcons[index * 2 + 1].addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14; // Getting first img width & adding 14 margin value
+        carousel.scrollLeft += firstImgWidth; // Scroll to the right
+        setTimeout(() => showHideIcons(), 60);
+    });
+
+    const dragStart = (e) => {
+        isDragStart = true;
+        prevPageX = e.pageX || e.touches[0].pageX;
+        prevScrollLeft = carousel.scrollLeft;
+    }
+
+    const dragging = (e) => {
+        if (!isDragStart) return;
+        e.preventDefault();
+        isDragging = true;
+        carousel.classList.add("dragging");
+        positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+        carousel.scrollLeft = prevScrollLeft - positionDiff;
+        showHideIcons();
+    }
+
+    const dragStop = () => {
+        isDragStart = false;
+        carousel.classList.remove("dragging");
+        if (!isDragging) return;
+        isDragging = false;
+        autoSlide();
+    }
+
+    const autoSlide = () => {
+        positionDiff = Math.abs(positionDiff);
+        let firstImgWidth = firstImg.clientWidth + 14;
+        let valDifference = firstImgWidth - positionDiff;
+        if (carousel.scrollLeft > prevScrollLeft) {
+            return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+        }
+        carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    }
+
+    carousel.addEventListener("mousedown", dragStart);
+    carousel.addEventListener("touchstart", dragStart);
+
+    carousel.addEventListener("mousemove", dragging);
+    carousel.addEventListener("touchmove", dragging);
+
+    carousel.addEventListener("mouseup", dragStop);
+    carousel.addEventListener("touchend", dragStop);
+});
