2a470bcf0938e423b114748b8be07bfb37adfb8f
feliceyovita <feliceyovitaaa@gmail.com>
2024-10-22 14:43:22 +0700

    perbaiki bookmark

--- category.php
+++ category.php
@@ -93,8 +93,11 @@ $stmt->bind_result($city_id, $city_name, $image_url, $tour_count);
                             <div class="bi bi-person-circle text-white fs-4 me-2"></div>
                             <ul>
                                 <li class="sub-item">
-                                    <i class="bi bi-bookmark material-icons-outlined"></i>
-                                    <p>Bookmark</p>
+                                    <a href="bookmark.html" class="bookmark-link" style="text-decoration: none; display: flex; align-items: center;">
+                                        <i class="bi bi-bookmark material-icons-outlined"></i>
+                                        <p style="margin-left: 8px;">Bookmark</p>
+                                    </a>
+                                </li>
                                 </li>
                                 <li class="sub-item">
                                     <i class="bi bi-box-arrow-left material-icons-outlined"></i>
